0d7f5cf6365a2a9e45bcd13cc580f3a20eb830fb
Kleber Martins <kleber@klsites.com.br>
2023-06-14 11:36:39 -0300

    Created project

---
+++ src/main/java/com/kl/advocatesystem/AdvocateSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kl.advocatesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvocateSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvocateSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kl/advocatesystem/AdvocateSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.advocatesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvocateSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
